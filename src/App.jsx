33362aa83a31e89aa0391bd403e6c588f121c19f
Kelvin Aulia Wilson <klvnnnyatiga@gmail.com>
2025-02-11 11:32:53 +0700

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout";
 import Dashboard from './pages/Dashboard';
 import AllPosts from './pages/posts/AllPosts';
+import AddPosts from "./pages/posts/AddPosts";
+import PreviewPosts from "./pages/posts/PreviewPosts";
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Layout><Dashboard /></Layout>} />
           <Route path="/posts" element={<Layout><AllPosts /></Layout>} />
+          <Route path="/posts/add" element={<Layout><AddPosts /></Layout>} />
           <Route path="/posts/preview" element={<Layout><PreviewPosts /></Layout>} />
         </Routes>
     </Router>
